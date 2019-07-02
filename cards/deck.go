5646824afe37ef4659acfaa778ad2a32c432c8e6
Robert Espinoza <sooperdooper@Roberts-MacBook-Pro.local>
2019-07-01 19:07:20 -0700

    write data to file

--- cards/deck.go
+++ cards/deck.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"strings"
+)
 
 // create a new type of 'deck'
 // which is a slice of strings
@@ -30,4 +34,12 @@ func (d deck) print() {
 
 func deal(d deck, handSize int) (deck, deck) {
 	return d[:handSize], d[handSize:]
+}
+
+func (d deck) toString() string {
+	return strings.Join([]string(d), ",")
+}
+
+func (d deck) saveToFile(filename string) error {
+	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
 }
