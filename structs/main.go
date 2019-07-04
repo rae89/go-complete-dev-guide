d6edcfde448a486be17708f071c9d087dcd5cbc0
Robert Espinoza <sooperdooper@Roberts-MacBook-Pro.local>
2019-07-03 23:06:53 -0700

    try pointers

--- structs/main.go
+++ structs/main.go
@@ -2,17 +2,35 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
 type person struct {
 	firstName string
 	lastName  string
+	contactInfo
 }
 
 func main() {
-	var alex person
+	jim := person{
+		firstName: "Jim",
+		lastName:  "Party",
+		contactInfo: contactInfo{
+			email:   "jim@gmail.com",
+			zipCode: 94000,
+		},
+	}
 
-	alex.firstName = "Alex"
-	alex.lastName = "Anderson"
+	jim.updateName("jimmy")
+	jim.print()
+}
 
-	fmt.Println(alex)
-	fmt.Printf("%+v", alex)
+func (pointerToPerson *person) updateName(newFirstName string) {
+	(*pointerToPerson).firstName = newFirstName
+}
+
+func (p person) print() {
+	fmt.Printf("%+v", p)
 }
